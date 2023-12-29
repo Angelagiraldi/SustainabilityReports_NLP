b057a6ee31f0bab9f148db158fecfc6312ffe87e
Angelagiraldi <giraldiangela@gmail.com>
2023-12-29 16:00:28 +0100

    correct usage of the parsers and its arguments

--- reports_NLP.py
+++ reports_NLP.py
@@ -10,9 +10,9 @@ pd.set_option("display.max_colwidth", None)
 
 
 nestle_url = "https://www.responsibilityreports.com/HostedData/ResponsibilityReports/PDF/OTC_NSRGY_2022.pdf"
-pp = ParsePDF(nestle_url)
-pp.extract_contents()
-sentences = pp.clean_text()
+pdf_parser = ParsePDF(nestle_url)
+content = pdf_parser.extract_contents()
+sentences = pdf_parser.clean_text(content)
 
 
 print(f"The McDonalds CSR report has {len(sentences):,d} sentences")
