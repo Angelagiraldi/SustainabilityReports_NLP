95220e07cd2cec82132374e5164605f77f3bdb6e
Angelagiraldi <giraldiangela@gmail.com>
2024-01-26 18:10:07 +0100

    Update InformationExtraction_LLM_RAG.py

--- InformationExtraction_LLM_RAG.py
+++ InformationExtraction_LLM_RAG.py
@@ -38,8 +38,71 @@ with open('config.yaml', 'r') as file:
 chunk_size = 1000
 
 # Initialize the pre-trained model
-model = AutoModelForCausalLM.from_pretrained(model=yaml_config['model'])
-tokenizer = AutoTokenizer.from_pretrained(model=yaml_config['model'])
+#################################################################
+# Tokenizer
+#################################################################
+
+model_name='mistralai/Mistral-7B-Instruct-v0.1'
+
+model_config = transformers.AutoConfig.from_pretrained(
+    model_name,
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+#################################################################
+# bitsandbytes parameters
+#################################################################
+
+# Activate 4-bit precision base model loading
+use_4bit = True
+
+# Compute dtype for 4-bit base models
+bnb_4bit_compute_dtype = "float16"
+
+# Quantization type (fp4 or nf4)
+bnb_4bit_quant_type = "nf4"
+
+# Activate nested quantization for 4-bit base models (double quantization)
+use_nested_quant = False
+
+#################################################################
+# Set up quantization config
+#################################################################
+compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=use_4bit,
+    bnb_4bit_quant_type=bnb_4bit_quant_type,
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=use_nested_quant,
+)
+
+# Check GPU compatibility with bfloat16
+if compute_dtype == torch.float16 and use_4bit:
+    major, _ = torch.cuda.get_device_capability()
+    if major >= 8:
+        print("=" * 80)
+        print("Your GPU supports bfloat16: accelerate training with bf16=True")
+        print("=" * 80)
+
+#################################################################
+# Load pre-trained config
+#################################################################
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    quantization_config=bnb_config,
+)
+inputs_not_chat = tokenizer.encode_plus("[INST] Tell me about fantasy football? [/INST]", return_tensors="pt")['input_ids'].to('cuda')
+
+generated_ids = model.generate(inputs_not_chat, 
+                               max_new_tokens=1000, 
+                               do_sample=True)
+decoded = tokenizer.batch_decode(generated_ids)
+
+
 
 # Process each document in collection
 for title in collection.list_doc_titles():
